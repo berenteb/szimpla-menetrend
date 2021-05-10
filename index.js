794cbe88c4a7c29a5ab0b80f7bb0beb2e6e74e5f
Bálint Berente <balint.berente@gmail.com>
2021-05-10 20:10:43 +0200

    Fixed bug on ssl settings

--- index.js
+++ index.js
@@ -19,13 +19,14 @@ app.use((err,req,res,next)=>{
     res.render("main", { view: 'error', data: err});
 })
 var server;
-if (process.env.SSL === true) {
+if (process.env.SSL === "true") {
     const key = fs.readFileSync(process.env.KEY_PATH);
     const cert = fs.readFileSync(process.env.CERT_PATH);
     server = https.createServer({ key: key, cert: cert }, app);
+    console.log("SSL cert and key loaded!");
 } else {
     server = http.createServer(app);
 }
 server.listen(process.env.PORT, () => {
-    console.log(process.env.SSL === true?"HTTPS":"HTTP" + " Server listening on " + process.env.PORT);
+    console.log((process.env.SSL === "true"?"HTTPS":"HTTP") + " Server listening on " + process.env.PORT);
 });
